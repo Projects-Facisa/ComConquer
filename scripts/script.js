704829d3fa824008811b051f632cf51cc69b89ef
svneto <severino.neto465@gmail.com>
2024-04-07 22:46:39 -0300

    fixed: footer not appearing in some cases, added folders for documents and modules, refactored teste class to page-content

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,12 @@ function playVideo(videoPath) {
     videoElement.src = videoPath;
     videoElement.style.display = "block";
   }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const PageContentElement = document.querySelector('.page-content');
+  const pageContainer = document.querySelector('.page-container');
+  
+  if (!PageContentElement) {
+      pageContainer.style.minHeight = 'calc(100vh - 82px)';
+  }
+  });
